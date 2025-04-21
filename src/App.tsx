74a9f248fdc29a3c71b2063c330f3ab68d19fa0c
Joy Huang <joydhuang@gmail.com>
2025-04-20 21:43:09 -0400

    add files

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import Button from "./components/Button";
+import Alert from "./components/Alert";
+import { useState } from "react";
+
+function App() {
+
+  const [alertVisible, setAlertVisibility]=useState(false);
+  
+  return (
+    <div>
+      {alertVisible && <Alert onClose={() => setAlertVisibility(false)}>My alert</Alert>}
+      <Button color="primary" onClick={() => setAlertVisibility(true)}>My Button</Button>
+
+    </div>
+  );
+}
+
+export default App;
